0df18f0f44e62fef4be4047784494c89ce94aca4
Irsan Jie <irsanjie@gmail.com>
2020-04-02 05:16:21 +0000

    bunyan

--- src/app.js
+++ src/app.js
@@ -4,4 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/listen', (req, res) => {
+    res.send('Hello World!'); 
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
