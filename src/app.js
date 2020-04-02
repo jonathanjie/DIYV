102a92222935d2442bec2997a8f0c68355d0230d
Irsan Jie <irsanjie@gmail.com>
2020-04-02 05:20:27 +0000

    remove bunyan

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-const Express = require('express');
+const Express = require("express");
+
+const log = Bunyan.createLogger({ name : "diyv-rest-psemillawebhook" });
+
 const app = Express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/listen', (req, res) => {
+    console.log("BODY", req.body);
     res.send('Hello World!'); 
-})
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
