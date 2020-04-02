de88c04add138fcad7a6d0d07a3bce9965c6a805
Irsan Jie <irsanjie@gmail.com>
2020-04-02 04:57:11 +0000

    bamboo

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const Express = require('express');
+const app = Express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
